1294d17125f8ce89d2bcc47daefd327c9ba84198
NIKHIL JI <taparianikhil13@gmail.com>
2021-12-02 20:52:14 +0530

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -1,36 +1,7 @@
-#include<iostream>
-using namespace std;
-
-class Test {
-public:
-
-    int checkArmstrong(int x) {
-        int r, num = 0;
-
-        while (x > 0) {
-            r = x % 10;
-            num = num + r * r* r;
-            x = x / 10;
-        }
-        return num;
-    }
-};
-
-int main() {
-
-    int x, arm;
-
-    cout << "Enter a number:";
-    cin >> x;
-
-    Test obj;
-    arm = obj.checkArmstrong(x);
-
-    if (arm == x) {
-        cout << "Number is armstrong:" << x; 
-    } else {
-        cout << "Number is not armstrong:" << x;
-    }
-
+#include<stdio.h>
+ main(){
+    enum tag(low='a',high='b',medium='c');
+    char try=low;
+    printf("%d",sizeof(tag));
     return 0;
 }
